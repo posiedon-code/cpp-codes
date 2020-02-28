20460c66e81ae37fa304d43cf33542c6ddc7f1d6
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2020-02-28 21:50:02 +0530

    Update and rename Stack.cpp to class based stack operartions.cpp

--- class based stack operartions.cpp
+++ class based stack operartions.cpp
@@ -7,41 +7,46 @@ using namespace std;
 
 //STACK class
 class STACK
-{	private:	class NODE
-				{	public:	int info;
-							NODE *link;
-							NODE(int a=-1);
-				};
-				NODE *hnode;
+{	
+	private:	class NODE
+			{	public:	int info;
+					NODE *link;
+					NODE(int a=-1);
+			};
+			NODE *hnode;
 
 	public:		STACK();
-				~STACK();
-				void push();
-				void pop();
-				void display();
+			~STACK();
+			void push();
+			void pop();
+			void display();
 };
 
 //NODE : parameterised contructor
 STACK::NODE::NODE(int a)
-{	info = a;
+{	
+	info = a;
 	link = NULL;
 }
 
 //STACK : constructor
 STACK::STACK()
-{	hnode = new NODE();
+{	
+	hnode = new NODE();
 }
 
 //STACK : destructor
 STACK::~STACK()
-{	delete hnode;
+{	
+	delete hnode;
 }
 
 
 
 //stack PUSH
 void STACK::push()
-{	NODE *q;
+{	
+	NODE *q;
 	NODE *pnode = hnode;
 	int val;
 	cout<<"\nEnter the value to be PUSHED: ";
@@ -51,7 +56,8 @@ void STACK::push()
 	pnode->link = q;
 	cout<<"\n";
 	for(int i=0;i<3;i++)
-	{	cout<<". ";
+	{	
+		cout<<". ";
 		Sleep(300);
 	}
 	cout<<" VALUE ENTERED";
@@ -62,19 +68,23 @@ void STACK::push()
 
 //stack POP
 void STACK::pop()
-{	NODE *pnode = hnode->link;
+{	
+	NODE *pnode = hnode->link;
 	NODE *dnode;
 	if(hnode->link == NULL)
-	{	cout<<"\n\nSTACK IS EMPTY ! DELETION NOT POSSIBLE !";
+	{	
+		cout<<"\n\nSTACK IS EMPTY ! DELETION NOT POSSIBLE !";
 	}
 	else
-	{	dnode = hnode->link;
+	{	
+		dnode = hnode->link;
 		pnode = pnode->link;
 		hnode->link =pnode;
 		delete dnode;
 		cout<<"\n";
 		for(int i=0;i<3;i++)
-		{	cout<<". ";
+		{	
+			cout<<". ";
 			Sleep(300);
 		}
 		cout<<" VALUE DELETED";
@@ -86,14 +96,17 @@ void STACK::pop()
 
 //stack DISPLAY
 void STACK::display()
-{	NODE *p;
+{	
+	NODE *p;
 	p = hnode->link;
 	if(p == NULL)
 		cout<<"\n STACK IS EMPTY !";
 	else
-	{	cout<<"\n STACK : ";
+	{	
+		cout<<"\n STACK : ";
 		while(p)
-		{	cout<<p->info<<" <- ";
+		{	
+			cout<<p->info<<" <- ";
 			p = p->link;
 		}
 		cout<<"BOTTOM";
@@ -107,10 +120,12 @@ void STACK::display()
 
 //main
 int main()
-{ 	STACK s;
+{ 	
+	STACK s;
 	int ch;
 	do
-	{	system("cls");
+	{	
+		system("cls");
 		cout<<"\n############################";
 		cout<<"\n#         MAIN MENU        #";
 		cout<<"\n############################";
@@ -121,39 +136,43 @@ int main()
 		cout<<"\n\n  >Enter choice : ";
 		cin>>ch;
 		switch(ch)
-		{	case 1:	s.push();
-					break;
+		{	
+			case 1:	s.push();
+				break;
 			
 			case 2:	s.pop();
-					break;
+				break;
 			
 			case 3:	system("cls");
-					s.display();
-					char ech;
-					while(1)
-					{	cout<<"\n\n>Execute MENU again ? (y/n)";
-						cin>>ech;
-						if(ech!='y' && ech!='n')
-						{	cout<<"\n INVALID INPUT !";
-							continue;
-						}
-						else
-							break;
+				s.display();
+				char ech;
+				while(1)
+				{	cout<<"\n\n>Execute MENU again ? (y/n)";
+					cin>>ech;
+					if(ech!='y' && ech!='n')
+					{	
+						cout<<"\n INVALID INPUT !";
+						continue;
 					}
-					if(ech == 'y')
-					{	system("cls");
+					else
 						break;
-					}
-					else if(ech == 'n')
-					{	cout<<"\n\n!! EXITING !!";
-						exit(0);
-					}
-			case 4:	cout<<"\n\nEXITING !!";
+				}
+				if(ech == 'y')
+				{	
+					system("cls");
+					break;
+				}
+				else if(ech == 'n')
+				{	
+					cout<<"\n\n!! EXITING !!";
 					exit(0);
+				}
+			case 4:	cout<<"\n\nEXITING !!";
+				exit(0);
 			
 			default:cout<<"\nINVALID INPUT !!";
-					Sleep(500);
-					continue;
+				Sleep(500);
+				continue;
 		}
 	}while(ch!=0);
 	return 0;
